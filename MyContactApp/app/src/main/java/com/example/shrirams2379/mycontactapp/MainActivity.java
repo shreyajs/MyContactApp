c9c16ec9702b12fb46ace124b6ff32b4c3807801
shreyajs <ShriramS2379@sduhsd.lan>
2017-05-12 09:24:24 -0700

    Updated DatabaseHelper and MainActivity to create Database
    Add Name Field and "add" button

--- MyContactApp/app/src/main/java/com/example/shrirams2379/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/shrirams2379/mycontactapp/MainActivity.java
@@ -4,14 +4,23 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    DatabaseHelper myDb;
+    EditText editName;
+    Button btnAddData;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myDb = new DatabaseHelper (this);
     }
 
     @Override
